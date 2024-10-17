b539b66cdfd076f7a13eaa6bc7953f71568e3df8
MegaAndi3000 <megaandi3000@gmail.com>
2024-10-17 21:38:59 +0200

    Fix directories not being created in destination

--- main.py
+++ main.py
@@ -37,14 +37,13 @@ def copy_item(source_path: str, destination_path: str, ignore_list: list):
 
     ignore_patterns = shutil.ignore_patterns(*ignore_list)
     if os.path.isdir(source_path):
-        if os.path.isdir(destination_path) is False:
-            shutil.copytree(source_path, destination_path, ignore=ignore_patterns)
-        else:
-            log_print("error", f"Directory already exists.")
+        destination_path = os.path.join(destination_path, os.path.basename(source_path))
+        shutil.copytree(source_path, destination_path, ignore=ignore_patterns)
     elif os.path.isfile(source_path):
         shutil.copy2(source_path, destination_path)
     else:
         log_print("error", f"Path '{source_path}' was not found.")
+
 
 def main():
 
